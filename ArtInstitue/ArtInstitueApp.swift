3068502b3ea1c67dc250ade30c2d0ca233544eff
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-12 11:21:13 +0200

    Initial Commit

---
+++ ArtInstitue/ArtInstitueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtInstitueApp.swift
+//  ArtInstitue
+//
+//  Created by Y K on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtInstitueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
